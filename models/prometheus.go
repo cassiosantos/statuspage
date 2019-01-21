849464f65e8453f67ddc9818df079cf1e11d0408
Renan Berto <renanbertoo@gmail.com>
2019-01-21 09:46:45 -0200

    Add Type to alerts

--- models/prometheus.go
+++ models/prometheus.go
@@ -3,12 +3,14 @@ package models
 import "time"
 
 type PrometheusIncomingWebhook struct {
-	Alerts []struct {
-		Status       string    `json:"status"`
-		Incident     Incident  `json:"labels" binding:"required"`
-		Component    Component `json:"annotations, omitempty"`
-		StartsAt     time.Time `json:"startsAt" time_format:"2006-01-02T15:04:05Z07:00"`
-		EndsAt       time.Time `json:"endsAt" time_format:"2006-01-02T15:04:05Z07:00"`
-		GeneratorURL string    `json:"generatorURL, omitempty"`
-	}
+	Alerts []PrometheusAlerts
+}
+
+type PrometheusAlerts struct {
+	Status       string    `json:"status"`
+	Incident     Incident  `json:"labels" binding:"required"`
+	Component    Component `json:"annotations, omitempty"`
+	StartsAt     time.Time `json:"startsAt" time_format:"2006-01-02T15:04:05Z07:00"`
+	EndsAt       time.Time `json:"endsAt" time_format:"2006-01-02T15:04:05Z07:00"`
+	GeneratorURL string    `json:"generatorURL, omitempty"`
 }
